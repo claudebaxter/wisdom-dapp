a518350e66c0facb828d343016a155ebec2a0f14
elborracho420 <e.baxter@dark-coin.io>
2022-07-20 16:18:36 -0500

    Initial draft of front end index.mjs

--- index.mjs
+++ index.mjs
@@ -8,3 +8,21 @@ const stdlib = loadStdlib(process.env); //Load the reach JS Stdlib for the conse
 console.log(`The consensus network is ${stdlib.connector}.`); //Display the consensus network type.
 
 const commonInteract = {}; // Define an empty (for now) common interaction object.
+
+// Seller
+if (role === 'seller') { //Code for when you run this app as the seller.
+    const sellerInteract = { //Define an empty (for now) Seller interaction object.
+        ...commonInteract,
+    }
+
+const acc = await stdlib.newTestAccount(stlib.parseCurrency(1000)); //Create an account for the seller. parseCurrency transforms units from standard to atomic.
+const ctc = acc.contract(backend); //Get a reference to the contract.
+await ctc.participants.Seller(sellerInteract); //Initiate interaction with contract for seller.
+
+// Buyer
+} else { //Code for when you run this app as the buyer
+    const buyerInteract = { //lines 24&25--Define empty (for now) Buyer interaction object.
+        ...commonInteract,
+    };
+
+};
