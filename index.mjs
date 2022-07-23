e8dd031cea3ef98708b50605d4ffd2b706ba23fb
elborracho420 <e.baxter@dark-coin.io>
2022-07-23 17:05:51 -0500

    Adding seller and buyer info, testing buy and sell interactions, adding pseudocode notes.

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import { loadStdlib } from '@reach-sh/stdlib'; //import the Reach JS Standard Library loader
 import * as backend from './build/index.main.mjs'; //import the JS backend compiled from index.rsh
-
+import { ask } from '@reach-sh/stdlib';
 if (process.argv.length < 3 || ['seller', 'buyer'].includes(process.argv[2]) == false) {
     console.log('Usage: reach run index [seller|buyer]');
     process.exit(0);
@@ -11,14 +11,10 @@ console.log(`Your role is ${role}`); //Display the role
 const stdlib = loadStdlib(process.env); //Load the reach JS Stdlib for the consensus network specified by REACH_CONNCECTOR_MODE env var.
 console.log(`The consensus network is ${stdlib.connector}.`); //Display the consensus network type.
 const suStr = stdlib.standardUnit;
-const auStr = stdlib.atomicUnit;
 const toAU = (su) => stdlib.parseCurrency(su);
 const toSU = (au) => stdlib.formatCurrency(au, 4);
-const suBal = 1000;
-console.log(`Balance is ${suBal} ${suStr}`);
-const auBal = toAU(suBal);
-console.log(`Balance is ${auBal} ${auStr}`);
-console.log(`Balance is ${toSU(auBal)} ${suStr}`);
+const iBalance = toAU(1000);
+const showBalance = async (acc) => console.log(`Your balance is ${toSU(await stdlib.balanceOf(acc))} ${suStr}.`);
 
 const commonInteract = {}; // Define an empty (for now) common interaction object.
 
@@ -26,16 +22,32 @@ const commonInteract = {}; // Define an empty (for now) common interaction objec
 if (role === 'seller') { //Code for when you run this app as the seller.
     const sellerInteract = { //Define an empty (for now) Seller interaction object.
         ...commonInteract,
-    }
+        price: toAU(5),
+        reportReady: async (price) => {
+            console.log(`Your wisdom is for sale at ${toSU(price)} ${suStr}.`);
+            console.log(`Contract info: ${JSON.stringify(await ctc.getInfo())}`);
+        },
+    };
 
-const acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000)); //Create an account for the seller. parseCurrency transforms units from standard to atomic.
+const acc = await stdlib.newTestAccount(iBalance); //Create an account for the seller. parseCurrency transforms units from standard to atomic.
+await showBalance(acc);
 const ctc = acc.contract(backend); //Get a reference to the contract.
 await ctc.participants.Seller(sellerInteract); //Initiate interaction with contract for seller.
+await showBalance(acc);
+
 
 // Buyer
 } else { //Code for when you run this app as the buyer
     const buyerInteract = { //lines 24&25--Define empty (for now) Buyer interaction object.
         ...commonInteract,
-    };
+        confirmPurchase: async (price) => await ask.ask(`Do you want to purchase wisdom for ${toSU(price)} ${suStr}?`, ask.yesno),
+    }; //ask.ask and ask.yesno are functions in @reach-sh/stdlib. ask.yesno accepts only y or n
+    const acc = await stdlib.newTestAccount(iBalance);
+    const info = await ask.ask('Paste contract info:', (s) => JSON.parse(s));
+    const ctc = acc.contract(backend, info);
+    await showBalance(acc); // You must parse contract information (so, it must be parsable).
+    await ctc.p.Buyer(buyerInteract); //You can substitute participants for p.
+    await showBalance(acc);
+}; //You can substitute participants for p.
 
-};
+ask.done(); // Needs to be after above closing tag so that it is accessible by both buyer and seller.
--- index.rsh
+++ index.rsh
@@ -1,17 +1,32 @@
 'reach 0.1';
 
 const commonInteract = {};
-const sellerInteract = {
-  ...commonInteract,
+const sellerInteract = { // user-defined Reach object
+  ...commonInteract,     // The spread syntax ... adds all commonInteract properties (none yet) to the object.
+  price: UInt,           // price is a UInt, a Reach-defined unsigned integer
+  reportReady: Fun([UInt], Null), // reportReady is a function that takes a UInt as an argument and returns nothing.
 };
-const buyerInteract = {
-  ...commonInteract,
+const buyerInteract = { //buyerInteract is a user-defined Reach object. 
+  ...commonInteract, //The spread syntax ... adds all commonInteract properties (none yet) to the object. 
+  confirmPurchase: Fun([UInt], Bool) //confirmPurchase is a function that takes a UInt and returns a Bool. 
 };
 
 export const main = Reach.App(() => {
   const S = Participant('Seller', sellerInteract);
   const B = Participant('Buyer', buyerInteract);
-  init();
+  init();               //init initializes the DApp, and transitions to a step.
   
-  exit();
+  S.only(() => { const price = declassify(interact.price); }); //S.only() transitions to a local step in which seller gets price. 
+  S.publish(price); //S.publish() transitions to a consensus step. 
+  S.interact.reportReady(price); //S.interact transitions to a local step in which seller passes price to frontend. 
+  commit(); //commit() transitions to a step. 
+
+  B.only (() => { const willBuy = declassify(interact.confirmPurchase(price)); }); //confirmPurchase passes price and returns true or false from frontend. 
+  B.publish(willBuy); //B.publish() transitions to a consensus step. 
+  if (!willBuy) {
+    commit(); //commit() transitions to a step. 
+    } else {
+      commit();
+    }
+  exit(); //halts the contract forever
 });
