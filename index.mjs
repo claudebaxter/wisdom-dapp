08a66f2138cecfb00d273078dd89abe12873ed25
elborracho420 <e.baxter@dark-coin.io>
2022-07-23 21:44:20 -0500

    Finishing dapp, adding purchase, transfer, and cxl functionality.

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import { loadStdlib } from '@reach-sh/stdlib'; //import the Reach JS Standard Library loader
 import * as backend from './build/index.main.mjs'; //import the JS backend compiled from index.rsh
 import { ask } from '@reach-sh/stdlib';
+
 if (process.argv.length < 3 || ['seller', 'buyer'].includes(process.argv[2]) == false) {
     console.log('Usage: reach run index [seller|buyer]');
     process.exit(0);
@@ -16,13 +17,22 @@ const toSU = (au) => stdlib.formatCurrency(au, 4);
 const iBalance = toAU(1000);
 const showBalance = async (acc) => console.log(`Your balance is ${toSU(await stdlib.balanceOf(acc))} ${suStr}.`);
 
-const commonInteract = {}; // Define an empty (for now) common interaction object.
+const commonInteract = (role) => ({ // Define a common interaction object.
+    reportCancellation: () => { console.log(`${role == 'buyer' ? 'You' : 'The buyer'} cancelled the order.`); },
+    reportPayment: (payment) => { console.log(`${role == 'buyer' ? 'You' : 'The buyer'} paid ${toSU(payment)} ${suStr} to the contract.`) },
+    reportTransfer: (payment) => { console.log (`The contract paid ${toSU(payment)} ${suStr} to ${role == 'seller' ? 'you' : 'the seller'}.`) }
+}); //Line 21 ${role == } defines output based on user / seller if cancelled by buyer.
 
 // Seller
 if (role === 'seller') { //Code for when you run this app as the seller.
     const sellerInteract = { //Define an empty (for now) Seller interaction object.
-        ...commonInteract,
+        ...commonInteract(role),
         price: toAU(5),
+        wisdom: await ask.ask('Enter a wise phrase, or press Enter for a default:', (s) => {
+            let w = !s ? 'Build healthy communities.' : s;
+            if (!s) { console.log(w); }
+            return w;
+        }),
         reportReady: async (price) => {
             console.log(`Your wisdom is for sale at ${toSU(price)} ${suStr}.`);
             console.log(`Contract info: ${JSON.stringify(await ctc.getInfo())}`);
@@ -35,16 +45,18 @@ const ctc = acc.contract(backend); //Get a reference to the contract.
 await ctc.participants.Seller(sellerInteract); //Initiate interaction with contract for seller.
 await showBalance(acc);
 
-
 // Buyer
 } else { //Code for when you run this app as the buyer
-    const buyerInteract = { //lines 24&25--Define empty (for now) Buyer interaction object.
-        ...commonInteract,
+    const buyerInteract = { // Buyer interaction object.
+        ...commonInteract(role),
         confirmPurchase: async (price) => await ask.ask(`Do you want to purchase wisdom for ${toSU(price)} ${suStr}?`, ask.yesno),
+        reportWisdom: (wisdom) => console.log(`Your new wisdom is "${wisdom}"`),
     }; //ask.ask and ask.yesno are functions in @reach-sh/stdlib. ask.yesno accepts only y or n
     const acc = await stdlib.newTestAccount(iBalance);
     const info = await ask.ask('Paste contract info:', (s) => JSON.parse(s));
     const ctc = acc.contract(backend, info);
+    const price = await ctc.views.Main.price();
+    console.log(`The price of wisdom is ${price[0] == 'None' ? '0' : toSU(price[1])} ${suStr}.`);
     await showBalance(acc); // You must parse contract information (so, it must be parsable).
     await ctc.p.Buyer(buyerInteract); //You can substitute participants for p.
     await showBalance(acc);
--- index.rsh
+++ index.rsh
@@ -1,32 +1,55 @@
 'reach 0.1';
 
-const commonInteract = {};
+const commonInteract = {
+  reportCancellation: Fun([], Null),
+  reportPayment: Fun([UInt], Null),
+  reportTransfer: Fun([UInt], Null)
+};
 const sellerInteract = { // user-defined Reach object
   ...commonInteract,     // The spread syntax ... adds all commonInteract properties (none yet) to the object.
   price: UInt,           // price is a UInt, a Reach-defined unsigned integer
+  wisdom: Bytes(128),
   reportReady: Fun([UInt], Null), // reportReady is a function that takes a UInt as an argument and returns nothing.
 };
 const buyerInteract = { //buyerInteract is a user-defined Reach object. 
   ...commonInteract, //The spread syntax ... adds all commonInteract properties (none yet) to the object. 
-  confirmPurchase: Fun([UInt], Bool) //confirmPurchase is a function that takes a UInt and returns a Bool. 
+  confirmPurchase: Fun([UInt], Bool), //confirmPurchase is a function that takes a UInt and returns a Bool. 
+  reportWisdom: Fun([Bytes(128)], Null)
 };
 
 export const main = Reach.App(() => {
   const S = Participant('Seller', sellerInteract);
   const B = Participant('Buyer', buyerInteract);
+  const V = View('Main', { price: UInt });
   init();               //init initializes the DApp, and transitions to a step.
   
   S.only(() => { const price = declassify(interact.price); }); //S.only() transitions to a local step in which seller gets price. 
   S.publish(price); //S.publish() transitions to a consensus step. 
   S.interact.reportReady(price); //S.interact transitions to a local step in which seller passes price to frontend. 
+  V.price.set(price);
   commit(); //commit() transitions to a step. 
 
   B.only (() => { const willBuy = declassify(interact.confirmPurchase(price)); }); //confirmPurchase passes price and returns true or false from frontend. 
   B.publish(willBuy); //B.publish() transitions to a consensus step. 
   if (!willBuy) {
     commit(); //commit() transitions to a step. 
+    each([S, B], () => interact.reportCancellation())
+    exit();
     } else {
       commit();
     }
-  exit(); //halts the contract forever
+  
+B.pay(price); //buyer always pays the contract
+each([S, B], () => interact.reportPayment(price)); //each calls interact.reportpayment for each participant in the array
+commit();
+
+S.only(() => { const wisdom = declassify(interact.wisdom); }); //After buyer commits to purchase the seller declassifies the wisdom
+S.publish(wisdom); //Seller makes the wisdom available to the buyer
+transfer(price).to(S);
+commit();
+
+each([S, B], () => interact.reportTransfer(price)); //each calls interact.reportTRansfer for each participant in the array
+B.interact.reportWisdom(wisdom); //Buyer sends the new wisdom to the frontend for the user.
+
+exit(); //halts the contract forever
 });
